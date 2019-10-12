b5066a64a419490f71cd245744552db30051e745
Carles <carles.gstocks@gmail.com>
2019-10-12 12:08:58 +0200

    Cards Setup

--- fly-filter/src/jsx/Cards.jsx
+++ fly-filter/src/jsx/Cards.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../css/Cards.css";
 import CityCard from "./CityCard";
+import FlyCard from "./FlyCard";
 
 export default class Cards extends Component {
     render() {
         return (
             <div className="cards_main">
                 <CityCard />
+                <FlyCard />
             </div>
         );
     }
