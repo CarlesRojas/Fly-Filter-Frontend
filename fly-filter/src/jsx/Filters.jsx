073772555e27aac2d01755c5aa516ce3938717d6
Carles <carles.gstocks@gmail.com>
2019-10-12 09:33:09 +0200

    Slider working!

--- fly-filter/src/jsx/Filters.jsx
+++ fly-filter/src/jsx/Filters.jsx
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
-import "../css/Filters.css"
-import SliderFilter from './SliderFilter'
+import React, { Component } from "react";
+import "../css/Filters.css";
+import SliderFilter from "./SliderFilter";
 
 export default class Filters extends Component {
     render() {
-        
         return (
             <div className="filters_main">
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
-                <SliderFilter />
+                <SliderFilter id={0} />
+                <SliderFilter id={1} />
+                <SliderFilter id={2} />
+                <SliderFilter id={3} />
+                <SliderFilter id={4} />
+                <SliderFilter id={5} />
+                <SliderFilter id={6} />
+                <SliderFilter id={7} />
+                <SliderFilter id={8} />
+                <SliderFilter id={9} />
+                <SliderFilter id={10} />
+                <SliderFilter id={11} />
+                <SliderFilter id={12} />
+                <SliderFilter id={13} />
+                <SliderFilter id={14} />
+                <SliderFilter id={15} />
+                <SliderFilter id={16} />
+                <SliderFilter id={17} />
             </div>
-        )
+        );
     }
 }
--- fly-filter/src/jsx/SliderFilter.jsx
+++ fly-filter/src/jsx/SliderFilter.jsx
@@ -1,72 +1,106 @@
-import React, { Component } from 'react'
-import "../css/SliderFilter.css"
-
+import React, { Component } from "react";
+import "../css/SliderFilter.css";
 
 export default class SliderFilter extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props);
+
         this.svgDOM = React.createRef();
         this.state = {
             sliderActive: false,
+            initial_x: 0,
             right: 0,
             left: 0,
             pointerEvents: "none"
-        }
+        };
     }
 
-    handleMouseDouwn = (event) => {
+    handleMouseDown = event => {
+        const { id } = this.props;
         event.preventDefault();
-        console.log("mouseDown");
-        this.svgDOM.current.addEventListener("mousemove", event => this.handleMouseMove(event));
+        document.getElementById("sliderFilter_svg_" + id).addEventListener("mousemove", this.handleMouseMove);
+
         var bounds = event.target.getBoundingClientRect();
-        var x = event.clientX - bounds.left;
-        var xr = -event.clientX + bounds.right;
+        var x1_from_left = event.clientX - bounds.left;
+        var x2_from_right = -event.clientX + bounds.right;
+
         this.setState({
             sliderActive: true,
-            left: x,
-            right: xr
-        })
-    }
-    
-    handleMouseUp = (event) => {
+            initial_x: x1_from_left,
+            left: x1_from_left,
+            right: x2_from_right
+        });
+    };
+
+    handleMouseUp = event => {
+        const { id } = this.props;
         event.preventDefault();
-        console.log("mouseUp");
-        this.svgDOM.current.removeEventListener("mousemove", event => this.handleMouseMove(event));
+        document.getElementById("sliderFilter_svg_" + id).removeEventListener("mousemove", this.handleMouseMove);
+
+        /*
         this.setState({
             pointerEvents: "all"
-        })
-    }
+        });
+        */
+    };
 
-    handleMouseMove = (event) => {
+    handleMouseMove = event => {
         event.preventDefault();
-        console.log("mouseMove");
+        const { initial_x } = this.state;
+
         var bounds = event.target.getBoundingClientRect();
-        var x = -event.clientX + bounds.right;
-        this.setState({
-            right: x
-        })
-    }
+        var x_from_left = event.clientX - bounds.left;
+        var initial_x_from_right = bounds.right - bounds.left - initial_x;
+        var x_from_right = -event.clientX + bounds.right;
+
+        if (x_from_left < initial_x) {
+            this.setState({
+                sliderActive: true,
+                right: initial_x_from_right,
+                left: x_from_left
+            });
+        } else {
+            this.setState({
+                sliderActive: true,
+                left: initial_x,
+                right: x_from_right
+            });
+        }
+    };
 
     render() {
+        const { sliderActive, right, left, pointerEvents } = this.state;
+        const { id } = this.props;
 
-        const { sliderActive, right, left, pointerEvents } = this.state
-        
         if (sliderActive) {
-            var sliderDOM = <div className="sliderFilter_handle" style={{right: right, left: left, pointerEvents: pointerEvents }}> 
-
-            </div>
+            var sliderDOM = <div className="sliderFilter_handle" style={{ right: right, left: left, pointerEvents: pointerEvents }}></div>;
         } else {
-            sliderDOM = null
+            sliderDOM = null;
         }
-        
-        return (       
+
+        return (
             <div className="sliderFilter_main">
-                <p className="sliderFilter_text">filter1</p>
-                <svg className="sliderFilter_svg" ref = {this.svgDOM} onMouseDown = {this.handleMouseDouwn} onMouseUp = {this.handleMouseUp}>
+                <p className="sliderFilter_text">Filter</p>
+                <svg className="sliderFilter_svg" id={"sliderFilter_svg_" + id}>
                     <line x1="0%" y1="50%" x2="100%" y2="50%" className="sliderFilter_line"></line>
                 </svg>
                 {sliderDOM}
             </div>
-        )
+        );
+    }
+
+    componentDidMount() {
+        const { id } = this.props;
+
+        // Sub to events when this component is mounted
+        document.getElementById("sliderFilter_svg_" + id).addEventListener("mousedown", this.handleMouseDown);
+        document.getElementById("sliderFilter_svg_" + id).addEventListener("mouseup", this.handleMouseUp);
+    }
+
+    // Stop listening to events
+    componentWillUnmount() {
+        const { id } = this.props;
+        document.getElementById("sliderFilter_svg_" + id).removeEventListener("mousedown", this.handleMouseDown);
+        document.getElementById("sliderFilter_svg_" + id).removeEventListener("mouseup", this.handleMouseUp);
     }
 }
