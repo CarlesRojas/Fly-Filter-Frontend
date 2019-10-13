d55dfeb52a3e01ba00e89b60532afb48a9d54458
Carles <carles.gstocks@gmail.com>
2019-10-13 07:53:19 +0200

    Hotels

--- fly-filter/src/jsx/HotelCard.jsx
+++ fly-filter/src/jsx/HotelCard.jsx
@@ -16,16 +16,22 @@ export default class HotelCard extends Component {
 
     render() {
         const { price } = this.props;
+
+        var hotels = require("../resources/hotels.json");
+        console.log(hotels);
+        var randomHotelIndex = Math.floor(Math.random() * (hotels.length - 1));
+        var numStars = Math.floor(Math.random() * 5) + 1;
+
         return (
             <div className="hotelcard_main">
                 <img className="hotelcard_icon" src={HotelIcon} alt="" />
-                <p className="hotelcard_name">Hotel Ars</p>
+                <p className="hotelcard_name">{hotels[randomHotelIndex]}</p>
 
-                <img className="hotelcard_star1" src={StarIcon} alt="" />
-                <img className="hotelcard_star2" src={StarIcon} alt="" />
-                <img className="hotelcard_star3" src={StarIcon} alt="" />
-                <img className="hotelcard_star4" src={StarIcon} alt="" />
-                <img className="hotelcard_star5" src={StarIcon} alt="" />
+                <img className={"hotelcard_star1" + (numStars < 1 ? " hotelcard_star_hidden" : "")} src={StarIcon} alt="" />
+                <img className={"hotelcard_star2" + (numStars < 2 ? " hotelcard_star_hidden" : "")} src={StarIcon} alt="" />
+                <img className={"hotelcard_star3" + (numStars < 3 ? " hotelcard_star_hidden" : "")} src={StarIcon} alt="" />
+                <img className={"hotelcard_star4" + (numStars < 4 ? " hotelcard_star_hidden" : "")} src={StarIcon} alt="" />
+                <img className={"hotelcard_star5" + (numStars < 5 ? " hotelcard_star_hidden" : "")} src={StarIcon} alt="" />
 
                 <p className="hotelcard_price">{price + " €"}</p>
                 <div className="hotelcard_point"></div>
